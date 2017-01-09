2f74e2f0211c6a52e0dfc48e00ac7ed688a599a2
Denis Isidoro <den.isidoro@gmail.com>
2017-01-09 10:18:21 -0200

    Fix infinite loading and add .localrc

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -4,7 +4,11 @@ alias v='nvim'
 
 # shortcuts
 alias c='clear'
-alias reload='env -i zsh' #'source ~/.zshrc'
+alias reload='source ~/.zshrc' # env -i zsh
 
 # git
 alias gcdr='cd $(git rev-parse --show-toplevel)'
+
+# browsing
+alias ls='ls --color=auto'
+alias lst="tree -L 2"
