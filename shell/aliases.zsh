3e05fab732acab8405953e42430a72ec19b3e7e9
Denis Isidoro <den.isidoro@gmail.com>
2017-01-18 16:29:07 -0200

    Add gcol

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -8,6 +8,14 @@ alias reload='source ~/.zshrc' # env -i zsh
 
 # git
 alias gcdr='cd $(git rev-parse --show-toplevel)'
+alias gpu='git push -u origin $(git rev-parse --abbrev-ref HEAD)'
+gcol() {
+  if [[ $# -eq 0 ]]; then
+    git branch --sort=-committerdate | head -n 6 | sed '/^\*/ d' | nl -nrz -w1 
+  else
+  	git checkout $(git branch --sort=-committerdate | sed '/^\*/ d' | awk -v n=$1 'NR == n')
+  fi
+}
 
 # browsing
 alias ls='ls --color=auto'
--- shell/plugins.zsh
+++ shell/plugins.zsh
@@ -1,5 +1,6 @@
 # syntax highlighting
 ZSH_HIGHLIGHT_STYLES[path]='bold'
+ZSH_HIGHLIGHT_STYLES[reserved-word]=none
 
 # autosuggestions
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
