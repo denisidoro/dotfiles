a48226e1d6556d0389a86517d5e114dbcd07d3a0
Denis Isidoro <denis_isidoro@live.com>
2017-03-19 20:45:50 -0300

    Improve install script organization

---
+++ scripts/aux/install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+brew_install() {
+  for repository in "$(from_dependencies "$@")"
+  do
+    brew install "$repository"
+  done
+}
+
+cask_install() {
+  for repository in "$(from_dependencies "$@")"
+  do
+    brew cask install "$repository"
+  done
+}
+
+gnu_install() {
+  brew tap homebrew/dupes
+  brew install binutils && brew install diffutils && brew install ed && brew install findutils && brew install gawk && brew install gnu-indent && brew install gnu-sed && brew install gnu-tar && brew install gnu-which && brew install gnutls && brew install grep && brew install gzip && brew install screen && brew install watch && brew install wdiff --with-gettext && brew install wget
+  brew install bash && brew install m4 && brew install make && brew install nano && brew install file-formula && brew install git && brew install less && brew install openssh && brew install rsync && brew install unzipi
+}
+
+update_dotfiles_common() {
+
+  DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../.. && pwd)"
+  CONFIG="$DOTFILES/scripts/conf.yaml"
+  DOTBOT_DIR="modules/dotbot"
+  DOTBOT_BIN="bin/dotbot"
+
+  echo "Setting folder architecture..."
+  mkdir -p ~/.config
+  mkdir -p ~/.config/{nvim,git}
+  pushd . > /dev/null
+  cd $DOTFILES
+
+  echo "Updating submodules..."
+  (git pull && git submodule init && git submodule update && git submodule status && \
+    cd "${DOTFILES}" && git submodule update --init --recursive "${DOTBOT_DIR}") > /dev/null
+
+  echo
+  "${DOTFILES}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${DOTFILES}" -c "${CONFIG}" "${@}" 
+  echo
+
+  # Neovim
+  echo "Installing neovim plugins..."
+  nvim +silent +PlugInstall +qall > /dev/null
+
+  # Tmux
+  echo "Installing tpm plugins..."
+  export TMUX_PLUGIN_MANAGER_PATH="$HOME/.tmux/plugins/"
+  bash "${TMUX_PLUGIN_MANAGER_PATH}tpm/bin/install_plugins" > /dev/null
+  bash "${TMUX_PLUGIN_MANAGER_PATH}tpm/bin/update_plugins" all > /dev/null
+
+  # ZPlug
+  echo "Attempting to install ZPlug plugins..."
+  zplug install 2> /dev/null
+
+  # Cleanup
+  popd > /dev/null
+
+}
+
+update_dotfiles_osx() {
+  echo "WIP"
+}
