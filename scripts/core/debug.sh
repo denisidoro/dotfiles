a27ae6667752b39423e1d137370e4102d4fb4502
Denis Isidoro <denis_isidoro@live.com>
2017-04-23 01:53:47 +0000

    Refactor log functions

--- scripts/core/debug.sh
+++ scripts/core/debug.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+freset=$(tput sgr0)
+
+purple=$(tput setaf 171)
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+tan=$(tput setaf 3)
+blue=$(tput setaf 38)
+
 readonly LOG_FILE="/tmp/$(basename "$0").log"
-info()    { echo -e "\e[32m[INF] $@\e[0m" | tee -a "$LOG_FILE" >&2 ; }
-warning() { echo -e "\e[93m[WRN] $@\e[0m" | tee -a "$LOG_FILE" >&2 ; }
-error()   { echo -e "\e[35m[ERR] $@\e[0m" | tee -a "$LOG_FILE" >&2 ; }
-fatal()   { echo -e "\e[91m[FTL] $@\e[0m" | tee -a "$LOG_FILE" >&2 ; exit 1 ; }
+
+function _log() {
+  local template=$1
+  shift
+  printf "$1" "$@" | tee -a "$LOG_FILE" >&2; 
+}
+
+function header() { _log "\n${bold}${purple}==========  %s  ==========${freset}\n" "$@"; }
+function success() { _log "${green}✔ %s${freset}\n" "$@"; }
+function error() { _log "${red}✖ %s${freset}\n" "$@"; }
+function warning() { _log "${tan}➜ %s${freset}\n" "$@"; }
+function note() { _log "${underline}${bold}${blue}Note:${freset} ${blue}%s${freset}\n" "$@"; }
+
