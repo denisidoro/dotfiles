feba8ce35050673c398bc05aeee4ff3285ecf490
Denis Isidoro <denis_isidoro@live.com>
2017-04-23 01:01:29 +0000

    Add version to scripts

--- scripts/core/documentation.sh
+++ scripts/core/documentation.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-function get_help_string() {
-  grep "^##?" "$1" | cut -c 5-
-}
-
-function eval_opts() {
-  local help="${1}"
-  shift
-  eval "$($DOTFILES/scripts/core/docopts -h "${help}" : "${@}")"
-}
-
 function eval_docopts() {
-  local help=$(get_help_string "$0")
-  eval_opts "$help" "$@"
+  local file="$0"
+  local help=$(grep "^##?" "$file" | cut -c 5-)
+  local version=$(grep "^#?"  "$file" | cut -c 4-)
+  eval "$($DOTFILES/scripts/core/docopts -h "${help}" -V "${version}" : "${@}")"
 }
 
