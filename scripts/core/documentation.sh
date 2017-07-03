59e2b3b81008442dee557b4dbe9f31263cd70ade
Denis Isidoro <denis_isidoro@live.com>
2017-07-03 13:32:48 -0300

    Refactor scripts + ./bin/dot

--- scripts/core/documentation.sh
+++ scripts/core/documentation.sh
@@ -3,7 +3,9 @@
 function eval_docopts() {
   local file="$0"
   local help=$(grep "^##?" "$file" | cut -c 5-)
+  local date=$(stat -f "%Sm" -t "%Y-%m-%d %H:%M" "$file")
   local version=$(grep "^ #?"  "$file" | cut -c 5-)
-  eval "$($DOTFILES/scripts/core/docopts -h "${help}" -V "${version}" : "${@}")"
+  eval "$($DOTFILES/scripts/core/docopts -h "${help}" -V "${version}
+${date}" : "${@}")"
 }
 
