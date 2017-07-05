b8756730178e7c681331098baba45201a994bf1a
Denis Isidoro <denis_isidoro@live.com>
2017-07-05 10:25:23 -0300

    Add git info to version

--- scripts/core/documentation.sh
+++ scripts/core/documentation.sh
@@ -3,9 +3,9 @@
 function eval_docopts() {
   local file="$0"
   local help=$(grep "^##?" "$file" | cut -c 5-)
-  local date=$(stat -f "%Sm" -t "%Y-%m-%d %H:%M" "$file")
-  local version=$(grep "^ #?"  "$file" | cut -c 5-)
-  eval "$($DOTFILES/scripts/core/docopts -h "${help}" -V "${version}
-${date}" : "${@}")"
+  local version_code=$(grep "^ #?"  "$file" | cut -c 5-)
+  local git_info=$(cd "$DOTFILES"; git log -n 1 --pretty=format:'%h%n%ad%n%an%n%s' --date=format:'%Y-%m-%d %Hh%M' -- "$file")
+  local version="$(echo -e "${version_code}\n${git_info}")"
+  eval "$($DOTFILES/scripts/core/docopts -h "${help}" -V "${version}" : "${@}")"
 }
 
