13750e7ba74b13a12ddf17285bbb14d6feced3ec
Denis Isidoro <denis_isidoro@live.com>
2017-07-05 13:39:21 -0300

    Fallback to 0.0.0 in case of unversioned script

--- scripts/core/documentation.sh
+++ scripts/core/documentation.sh
@@ -3,8 +3,8 @@
 function eval_docopts() {
   local file="$0"
   local help=$(grep "^##?" "$file" | cut -c 5-)
-  local version_code=$(grep "^ #?"  "$file" | cut -c 5-)
-  local git_info=$(cd "$DOTFILES"; git log -n 1 --pretty=format:'%h%n%ad%n%an%n%s' --date=format:'%Y-%m-%d %Hh%M' -- "$file")
+  local version_code=$(grep "^ #?"  "$file" | cut -c 5- || echo "0.0.0")
+  local git_info=$(cd "$DOTFILES"; git log -n 1 --pretty=format:'%h%n%ad%n%s' --date=format:'%Y-%m-%d %Hh%M' -- "$file")
   local version="$(echo -e "${version_code}\n${git_info}")"
   eval "$($DOTFILES/scripts/core/docopts -h "${help}" -V "${version}" : "${@}")"
 }
