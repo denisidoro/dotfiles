7ff8c35bbad89fa17cba10745888af8390063fd0
Denis Isidoro <denis_isidoro@live.com>
2017-07-31 11:13:50 -0300

    Fix dot xargs bug

--- scripts/core/osx_gnu.sh
+++ scripts/core/osx_gnu.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 if is_osx && command_exists ggrep; then
-  function sed(){ gsed "$@"; }
-  function awk(){ gawk "$@"; }
-  function find(){ gfind "$@"; }
-  function grep(){ ggrep "$@"; }
-  function head(){ ghead "$@"; }
-  function mktemp(){ gmktemp "$@"; }
-  function date(){ gdate "$@"; }
-  function shred(){ gshred "$@"; }
-  function cut(){ gcut "$@"; }
-  function tr(){ gtr "$@"; }
-  function od(){ god "$@"; }
-  function cp(){ gcp "$@"; }
-  function cat(){ gcat "$@"; }
-  function sort(){ gsort "$@"; }
-  function kill(){ gkill "$@"; }
-  export -f sed awk find head mktemp date shred cut tr od cp cat sort kill
+  function sed() { gsed "$@"; }
+  function awk() { gawk "$@"; }
+  function find() { gfind "$@"; }
+  function grep() { ggrep "$@"; }
+  function head() { ghead "$@"; }
+  function mktemp() { gmktemp "$@"; }
+  function date() { gdate "$@"; }
+  function shred() { gshred "$@"; }
+  function cut() { gcut "$@"; }
+  function tr() { gtr "$@"; }
+  function od() { god "$@"; }
+  function cp() { gcp "$@"; }
+  function cat() { gcat "$@"; }
+  function sort() { gsort "$@"; }
+  function kill() { gkill "$@"; }
+  function xargs() { gxargs "$@"; }
+  export -f sed awk find head mktemp date shred cut tr od cp cat sort kill xargs
 fi
 
