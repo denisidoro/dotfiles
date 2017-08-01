eca34fefa41702c120770bbb11a4c50ddf34a65a
Denis Isidoro <denis_isidoro@live.com>
2017-08-01 19:03:35 -0300

    Add fzf selection to dot

--- scripts/core/documentation.sh
+++ scripts/core/documentation.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+function extract_help() {
+  local file="$1"
+  grep "^##?" "$file" | cut -c 5-
+}
+
 function eval_docopts() {
   local file="$0"
-  local help=$(grep "^##?" "$file" | cut -c 5-)
+  local help=$(extract_help "$file")
   local version_code=$(grep "^ #?"  "$file" | cut -c 5- || echo "0.0.0")
   local git_info=$(cd "$DOTFILES"; git log -n 1 --pretty=format:'%h%n%ad%n%s' --date=format:'%Y-%m-%d %Hh%M' -- "$file")
   local version="$(echo -e "${version_code}\n${git_info}")"
