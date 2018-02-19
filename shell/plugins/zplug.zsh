00aea9d0271526d7330cc9af295acf6866d906aa
Denis Isidoro <denis_isidoro@live.com>
2018-02-19 10:14:32 -0300

    Random minor fixes

--- shell/plugins/zplug.zsh
+++ shell/plugins/zplug.zsh
@@ -3,12 +3,12 @@ source $ZPLUG_HOME/init.zsh
 # Bundles from oh-my-zsh
 zplug 'robbyrussell/oh-my-zsh', use:'lib/*'
 zplug "plugins/git", from:oh-my-zsh
-zplug "plugins/tmuxinator", from:oh-my-zsh
-zplug "plugins/shrink-path", from:oh-my-zsh
+#zplug "plugins/tmuxinator", from:oh-my-zsh
+#zplug "plugins/shrink-path", from:oh-my-zsh
 
 # Writing
 zplug zsh-users/zsh-autosuggestions
-zplug zsh-users/zsh-history-substring-search
+# zplug zsh-users/zsh-history-substring-search
 zplug zsh-users/zsh-syntax-highlighting
 
 # Versioning
@@ -17,7 +17,7 @@ zplug zsh-users/zsh-syntax-highlighting
 #zplug "shyiko/commacd", use:"commacd.bash"
 
 # Load the theme
-setopt prompt_subst
+# setopt prompt_subst
 #zplug "yardnsm/blox-zsh-theme", use:blox.zsh-theme, defer:3
 zplug denysdovhan/spaceship-zsh-theme, use:spaceship.zsh, from:github, as:theme
 
@@ -36,8 +36,6 @@ zplug load
 #function prompt_short_dir { echo "%{$fg_bold[yellow]%}$(shrink_path -f)" }
 #PROMPT="$(echo $PROMPT | sed -e 's/\$prompt_dir/\$(prompt_short_dir)/')"
 
-
-
 # ORDER
 SPACESHIP_PROMPT_ORDER=(
   time          # Time stampts section
@@ -45,13 +43,8 @@ SPACESHIP_PROMPT_ORDER=(
   host          # Hostname section
   dir           # Current directory section
   git           # Git section (git_branch + git_status)
-  package       # Package version
-  node          # Node.js section
-  docker        # Docker section
   exec_time     # Execution time
   line_sep      # Line break
-  vi_mode       # Vi-mode indicator
-  jobs          # Backgound jobs indicator
   exit_code     # Exit code section
   char          # Prompt character
 )
