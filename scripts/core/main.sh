b544af0dfe03ee854ce3718c0856d33776d201bb
Denis Isidoro <denis_isidoro@live.com>
2018-07-20 14:42:07 -0300

    random changes

--- scripts/core/main.sh
+++ scripts/core/main.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-source "${DOTFILES}/scripts/core/platform.sh"
-source "${DOTFILES}/scripts/core/osx_gnu.sh"
-source "${DOTFILES}/scripts/core/collections.sh"
-source "${DOTFILES}/scripts/core/debug.sh"
-source "${DOTFILES}/scripts/core/dependencies.sh"
-source "${DOTFILES}/scripts/core/documentation.sh"
-source "${DOTFILES}/scripts/core/filesystem.sh"
-source "${DOTFILES}/scripts/core/string.sh"
-source "${DOTFILES}/scripts/core/feedback.sh"
-
+if [ -z ${DOT_MAIN_IMPORTED+x} ] || ! "${DOT_MAIN_IMPORTED}" ; then
+  source "${DOTFILES}/scripts/core/platform.sh"
+  source "${DOTFILES}/scripts/core/osx_gnu.sh"
+  source "${DOTFILES}/scripts/core/collections.sh"
+  source "${DOTFILES}/scripts/core/debug.sh"
+  source "${DOTFILES}/scripts/core/dependencies.sh"
+  source "${DOTFILES}/scripts/core/documentation.sh"
+  source "${DOTFILES}/scripts/core/filesystem.sh"
+  source "${DOTFILES}/scripts/core/string.sh"
+  source "${DOTFILES}/scripts/core/feedback.sh"
+  readonly DOT_MAIN_IMPORTED=true
+fi
