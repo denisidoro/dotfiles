0ff845956cfb879234024d54e7f0ce0b11e20d2a
Denis Isidoro <denis_isidoro@live.com>
2018-08-30 12:28:24 -0300

    Use docoptsh

--- scripts/core/documentation.sh
+++ scripts/core/documentation.sh
@@ -7,12 +7,16 @@ function extract_help() {
 
 function docs::eval() {
     local readonly file="$0"
-    help="$(extract_help "$file")"
-    docopts="$DOTFILES/scripts/core/docopts"
+    local readonly help="$(extract_help "$file")"
+    if platform::command_exists "python"
+      docopts="$DOTFILES/scripts/core/docopts"
+    else
+      docopts="$DOTFILES/modules/docoptsh/docoptsh"
+    fi
     if [[ ${1:-} = "--version" ]]; then
         local readonly version_code=$(grep "^ #?" "$file" | cut -c 5- || echo "unversioned")
-        git_info=$(cd "$(dirname "$file")" && git log -n 1 --pretty=format:'%h%n%ad%n%an%n%s' --date=format:'%Y-%m-%d %Hh%M' -- "$(basename "$file")")
-        version="$(echo -e "${version_code}\n${git_info}")"
+        local readonly git_info=$(cd "$(dirname "$file")" && git log -n 1 --pretty=format:'%h%n%ad%n%an%n%s' --date=format:'%Y-%m-%d %Hh%M' -- "$(basename "$file")")
+        local readonly version="$(echo -e "${version_code}\n${git_info}")"
         eval "$($docopts -h "${help}" -V "${version}" : "${@:1}")"
     else
         eval "$($docopts -h "${help}" : "${@:1}")"
