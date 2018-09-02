b0094a1e21a413a278adb778de061352668d9461
Denis Isidoro <denis_isidoro@live.com>
2018-09-01 21:53:11 -0300

    Use docoptsh if necessary

--- scripts/core/documentation.sh
+++ scripts/core/documentation.sh
@@ -8,7 +8,13 @@ function extract_help() {
 function docs::eval() {
     local readonly file="$0"
     help="$(extract_help "$file")"
-    docopts="$DOTFILES/scripts/core/docopts"
+
+    if [[ ${DOTFILES_DOCOPTSH:-false} == true ]]; then
+        docopts="$DOTFILES/modules/docoptsh/docoptsh"
+    else
+        docopts="$DOTFILES/scripts/core/docopts"
+    fi
+    
     if [[ ${1:-} == "--version" ]]; then
         local readonly version_code=$(grep "^#??" "$file" | cut -c 5- || echo "unversioned")
         git_info=$(cd "$(dirname "$file")" && git log -n 1 --pretty=format:'%h%n%ad%n%an%n%s' --date=format:'%Y-%m-%d %Hh%M' -- "$(basename "$file")")
