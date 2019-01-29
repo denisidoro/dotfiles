7ff4d4fa0f1adf1e6f20e9004d29255c0dc401b1
Denis Isidoro <denisidoro@users.noreply.github.com>
2019-01-29 17:43:59 -0200

    Windows improvements (#28)

--- scripts/core/dependencies.sh
+++ scripts/core/dependencies.sh
@@ -7,23 +7,3 @@ function rsrc() {
 function script_dir() {
    cd "$(dirname "${BASH_SOURCE[0]}")" && pwd
 }
-
-function deps::read() {
-   local file
-   file=$(rsrc "db/dependencies.txt")
-   echo "$(cat "$file")\n\n"
-}
-
-function deps::from() {
-   # install compatible grep if necessary
-   if platform::is_osx; then
-      if ! platform::command_exists ggrep; then
-         brew tap homebrew/dupes
-         brew install homebrew/dupes/grep
-      fi
-   fi
-
-   for key in "$@"; do
-      echo "$dependencies" | grep -Pzo "^$key:\n(.|\n)*?\n{2}" | tail -n +2 | sed '/^$/d'
-   done
-}
