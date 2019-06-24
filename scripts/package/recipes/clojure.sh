d6774493e7c66431a4a960ca838ad8b586ef472f
Denis Isidoro <denis_isidoro@live.com>
2019-06-24 16:46:46 -0300

    fix

--- scripts/package/recipes/clojure.sh
+++ scripts/package/recipes/clojure.sh
@@ -6,11 +6,9 @@ source "${DOTFILES}/scripts/package/aux/recipes.sh"
 
 recipe::abort_if_installed clojure
 
-pm="$(recipe::main_package_manager)"
-
-if [[ $pm = "brew" ]]; then
-	brew install clojure
-esac
+dot pkg add  clojure \
+   && recipe::abort_if_installed clojure \
+   || true
 
 CLJ_VERSION="1.10.1.447"
 CLJ_FOLDER="$TEMP_FOLDER/clojure"
