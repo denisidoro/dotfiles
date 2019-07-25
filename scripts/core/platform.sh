e0f54005dc7a933f4f2ec9c79d83fee6c032f73a
Denis Isidoro <denis_isidoro@live.com>
2019-07-24 22:07:53 -0300

    wip

--- scripts/core/platform.sh
+++ scripts/core/platform.sh
@@ -53,3 +53,26 @@ platform::tags() {
    if platform::is_wsl; then tags="${tags}wsl "; fi
    echo "$tags"
 }
+
+platform::main_package_manager() {
+   if platform::is_osx; then
+      echo "brew"
+   elif platform::is_android; then
+      echo "pkg"
+   elif platform::command_exists apt; then
+      echo "apt"
+   elif platform::command_exists apt-get; then
+      local readonly apt_get_path="$(which apt-get)"
+      local readonly apt_path="$(echo "$apt_get_path" | sed 's/-get//')"
+      sudo ln -s "$apt_get_path" "$apt_path"
+      echo "apt"
+   elif platform::command_exists yum; then
+      echo "yum"
+   elif platform::command_exists dnf; then
+      echo "dnf"
+   elif platform::command_exists apk; then
+      echo "apk"
+   else
+      echo "brew"
+   fi
+}
