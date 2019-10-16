5453e57788ac976a1239cc68281e3b7fa645a119
Denis Isidoro <denis_isidoro@live.com>
2019-10-16 18:17:53 -0300

    fix

--- scripts/package/recipes/bash-yaml.sh
+++ scripts/package/recipes/bash-yaml.sh
@@ -2,10 +2,13 @@
 # vim: filetype=sh
 set -euo pipefail
 
+user="jasperes"
+repo="bash-yaml"
+
 bash-yaml::is_installed() {
    recipe::has_submodule $repo "script/yaml.sh"
 }
 
 bash-yaml::install() {
-   recipe::clone_as_submodule jasperes bash-yaml
+   recipe::clone_as_submodule $user $repo
 }
