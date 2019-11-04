079b0294724962b5b24370ae3e0ae8015738a246
Denis Isidoro <denis_isidoro@live.com>
2019-11-04 15:24:04 -0300

    lint

--- tasker/sh/wifi.sh
+++ tasker/sh/wifi.sh
@@ -2,35 +2,35 @@ TASKER_HOME="${TASKER_HOME:-/sdcard/Tasker}"
 source "${TASKER_HOME}/sh/core.sh"
 
 network_name() {
-  echo "$*" \
-    | coll::get 2 \
-    | str::lowercase
+   echo "$*" \
+      | coll::get 2 \
+      | str::lowercase
 }
 
 is_connected() {
-  echo "$*" \
-    | head -n1 \
-    | grep -q CONNECT
+   echo "$*" \
+      | head -n1 \
+      | grep -q CONNECT
 }
 
 group_for_network() {
-  local -r text="$1"
-  local -r network="$2"
+   local -r text="$1"
+   local -r network="$2"
 
-  echo "$text" \
-    | grep "$network" -B 90 \
-    | tac \
-    | grep '\[' \
-    | head -n1 \
-    | tr -d '[' \
-    | tr -d ']'
+   echo "$text" \
+      | grep "$network" -B 90 \
+      | tac \
+      | grep '\[' \
+      | head -n1 \
+      | tr -d '[' \
+      | tr -d ']'
 }
 
 group_name() {
-  local -r info="$1"
-  local -r text="$2"
-  local -r network="$(network_name "$info")"
-  group_for_network "$text" "$(network_name "$text")"
+   local -r info="$1"
+   local -r text="$2"
+   local -r network="$(network_name "$info")"
+   group_for_network "$text" "$(network_name "$text")"
 }
 
 info="%WIFII"
