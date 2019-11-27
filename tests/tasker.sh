26eddb85d5da3160ff5911fad10ed319672aa4c5
Denis Isidoro <denis_isidoro@live.com>
2019-11-27 14:51:55 -0300

    more tests

--- tests/tasker.sh
+++ tests/tasker.sh
@@ -11,7 +11,7 @@ wifi_from_info() {
    local -r expected="$2"
 
    local -r txt="$(echo -e "[home]\ncasa\ncasita\nmaison\n\n[work]\ntrabalho\nboulot")"
-   local -r info="$(echo -e ">>>> CONNECTION <<<<<\n\n${network}\n\nMac:123\nIP:456")"
+   local -r info="$(echo -e ">>>>> CONNECTION <<<<<\n\n${network}\n\nMac:123\nIP:456")"
 
    _group_name "$info" "$txt" \
     | test::equals "$expected"
@@ -22,3 +22,4 @@ test::run "wifi from info - work" wifi_from_info "boulot" "work"
 test::run "wifi from info - home" wifi_from_info "casita" "home"
 test::run "wifi from info - home" wifi_from_info "casa" "home"
 test::run "wifi from info - home" wifi_from_info "maison" "home"
+test::run "wifi from info - home" wifi_from_info "foooo" "home"
