1344aa99ca29f10358f0b4a7a2b50c6127283277
Denis Isidoro <denis.isidoro@uber.com>
2020-05-24 10:53:01 -0300

    wip

--- tests/cargo.sh
+++ tests/cargo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 _cargo_test() {
-    cd "${DOTFILES}/rust"
-    cargo test
+   cd "${DOTFILES}/rust"
+   cargo test
 }
 
 test::set_suite "rust - cargo"
--- tests/transpile.sh
+++ tests/transpile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-_gen_source() {
+_gen_go_source() {
    local -r source="$1"
    cat << EOB > "$source"
 package main
@@ -18,9 +18,9 @@ func main() {
 EOB
 }
 
-_transpile_error_propagation() {
+_transpile_go_error_propagation() {
    local -r source="${TEST_DIR}/code.go"
-   _gen_source "$source"
+   _gen_go_source "$source"
    dot code transpile "$source"
    local -r result="$(cat "$source")"
    echo "$result" | test::includes "foo, err := myFn()" \
@@ -28,15 +28,38 @@ _transpile_error_propagation() {
       && echo "$result" | test::includes "if err != nil {"
 }
 
-_transpile_map() {
+_transpile_go_map() {
    local -r source="${TEST_DIR}/code.go"
-   _gen_source "$source"
+   _gen_go_source "$source"
    dot code transpile "$source"
    local -r result="$(cat "$source")"
    echo "$result" | test::includes "pies := make([]Pie, len(apples))" \
       && echo "$result" | test::includes "pies[i] = halfBakedPie.finish()"
 }
 
+_gen_bash_source() {
+   local -r source="$1"
+   cat << EOB > "$source"
+myfn(arg1, arg2, arg3) {
+   body
+}
+EOB
+}
+
+_transpile_bash_args() {
+   local -r source="${TEST_DIR}/code.bash"
+   _gen_bash_source "$source"
+   dot code transpile "$source"
+   local -r result="$(cat "$source")"
+   echo "$result" | test::includes "myfn() {" \
+      && echo "$result" | test::includes "local -r arg1" \
+      && echo "$result" | test::includes "local -r arg2" \
+      && echo "$result" | test::includes "local -r arg3"
+}
+
 test::set_suite "rust - go"
-test::run "transpile - error propagation" _transpile_error_propagation
-test::skip "transpile - map" _transpile_map
+test::run "transpile - error propagation" _transpile_go_error_propagation
+test::skip "transpile - map" _transpile_go_map
+
+test::set_suite "rust - bash"
+test::run "transpile - args" _transpile_bash_args
