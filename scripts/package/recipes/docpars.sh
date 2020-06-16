8e684dde3b9be863d8189552b6469229d95100d8
Denis Isidoro <denis.isidoro@uber.com>
2020-06-16 10:28:04 -0300

    Jun-16 10h28: changelog, proxy, docpars.sh,

--- scripts/package/recipes/docpars.sh
+++ scripts/package/recipes/docpars.sh
@@ -2,7 +2,5 @@
 set -euo pipefail
 
 package::install() {
-   package::command_exists brew && brew install denisidoro/tools/docpars && return 0 || true
-
-   bash <(curl -sL https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install)
+   package::command_exists brew && brew install denisidoro/tools/docpars
 }
