35b3496e7613a5a493399f48b73bf527c9cc7e64
Denis Isidoro <denis.isidoro@uber.com>
2020-11-09 10:45:00 -0300

    Nov-09 10h45: entry_point.bash, p10k.zsh, shortcut

--- termux/tasker/entry_point.bash
+++ termux/tasker/entry_point.bash
@@ -3,32 +3,32 @@ export SDCARD_TASKER="${SDCARD_TASKER:-/sdcard/Tasker}"
 export SDCARD_DOTFILES="${SDCARD_DOTFILES:-/sdcard/dotfiles}"
 
 _script() {
-   exit 3 
+   exit 3
 }
 
 _dot() {
-    "${DOTFILES}/bin/dot" "$@"
+   "${DOTFILES}/bin/dot" "$@"
 }
 
 _eval() {
-    eval "$*"
+   eval "$*"
 }
 
 _handler() {
-    local -r cmd="${1:-}"
-    shift
-    case "$cmd" in
-        eval) _eval "$@" ;;
-        script) _script "$@" ;;
-        dot) _dot "$@" ;;
-        *) exit 2 ;;
-    esac
+   local -r cmd="${1:-}"
+   shift
+   case "$cmd" in
+      eval) _eval "$@" ;;
+      script) _script "$@" ;;
+      dot) _dot "$@" ;;
+      *) exit 2 ;;
+   esac
 }
 
 _main() {
-  IFS=$'\n'
-  args=($(eval "for arg in $*; do echo \$arg; done"))
-  _handler "${args[@]:-}"
+   IFS=$'\n'
+   args=($(eval "for arg in $*; do echo \$arg; done"))
+   _handler "${args[@]:-}"
 }
 
 _main "$@" > "${SDCARD_TASKER}/sh/out.txt" 2> "${SDCARD_TASKER}/sh/err.txt"
