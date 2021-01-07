3f05cae1b66cc2ec3c4832d55a6a3fc60eefd498
Denis Isidoro <denis.isidoro@uber.com>
2021-01-07 11:14:15 -0300

    Jan-07 11h14: abra, abra.sh, score, ...

--- scripts/package/recipes/abra.sh
+++ scripts/package/recipes/abra.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 package::install() {
-    has brew && brew install denisidoro/tools/abra && return 0 || true
+   has brew && brew install denisidoro/tools/abra && return 0 || true
 
    GITHUB_USERNAME="denisidoro" REPO="abra" dot rust binary download
 }
