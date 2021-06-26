17885e63234bf36fb7f608b31c47ab51c0cf8789
Denis Isidoro <denis.isidoro@uber.com>
2021-06-26 07:44:51 -0300

    Jun-26 07h44: vars.bash, config

--- shell/aux/vars.bash
+++ shell/aux/vars.bash
@@ -26,7 +26,6 @@ export LEIN_SUPPRESS_USER_LEVEL_REPO_WARNINGS=true
 # Golang
 export GOPATH="${DEV_HOME}/go"
 export GOBIN="${GOPATH}/bin"
-export GOPROXY="https://goproxy.uberinternal.com|https://goproxy.uberinternal.com"
 
 # package management
 export HOMEBREW_NO_AUTO_UPDATE=1
